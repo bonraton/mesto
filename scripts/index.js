a15d6d39fd84e4c4dec3211994f8f598aab25694
bonraton <77760523+bonraton@users.noreply.github.com>
2021-03-22 04:27:39 +0300

    fix: исправить стили и скрипт

---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+// переменные для попапа и кнопок
+let popup = document.querySelector('.popup');
+let profile = document.querySelector('.profile');
+let profileEditBtn = profile.querySelector('.profile__edit-btn');
+let popupCloseBtn = popup.querySelector('.popup-form__close-btn');
+let popupSubmitBtn = popup.querySelector('.popup-form__submit');
+
+// Переменные полей ввода
+let profileNameInput = popup.querySelector('.popup-form__input_name');
+let profileDescriptionInput = popup.querySelector('.popup-form__input_description');
+
+
+//функции для добавления и удаленния класса в попап.
+function popupVisible() {
+    popup.classList.add('popup_visible');
+    profileNameInput.value = profileName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+}
+
+function popupHidden() {
+    popup.classList.remove('popup_visible');
+}
+
+// вызов функциий  по клику
+profileEditBtn.addEventListener('click', popupVisible);
+popupCloseBtn.addEventListener('click', popupHidden);
+
+
+
+// Переменные для сохранения введенных данных
+let profileName = profile.querySelector('.profile__name');
+let profileDescription = profile.querySelector('.profile__description');
+
+//функция для сохранения введеных данных в профиль
+
+function submitData() {
+    profileName.textContent = profileNameInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    submitData()
+    popupHidden();
+}
+
+popupSubmitBtn.addEventListener('click', formSubmitHandler);
